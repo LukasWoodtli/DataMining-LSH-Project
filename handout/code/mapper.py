a57b0073f5fe211df8fcb8251bae742113f657f3
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-10-06 21:55:07 +0200

    Work on mapper for LSH

--- handout/code/mapper.py
+++ handout/code/mapper.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 
 
 NUM_OF_HASH_FUNCTIONS = 100
+MAX_HASH_VAL = 2**32 - 1
+ROWS_PER_BAND = 7
 
 if __name__ == "__main__":
     # VERY IMPORTANT:
@@ -14,12 +16,19 @@ if __name__ == "__main__":
     # same seed when generating random numbers for the hash functions.
     np.random.seed(seed=42)
 
+    # create hash functions
+    hash_functions = []
+    for i in range(NUM_OF_HASH_FUNCTIONS):
+        a = np.random.random_integers(0, MAX_HASH_VAL)
+        b = np.random.random_integers(0, MAX_HASH_VAL)
+        hash_functions.append(lambda x: (a * hash(x)  + b) % MAX_HASH_VAL)
+
     shingles_in_video = {}
     all_shingles = np.array([], dtype=int)
 
     for line in sys.stdin:
         line = line.strip()
-        
+
         # get video id and shingles for video
         video_id = int(line[6:15])
         shingles = np.fromstring(line[16:], dtype=int, sep=" ")
@@ -27,31 +36,19 @@ if __name__ == "__main__":
         assert video_id not in shingles_in_video, "Same video ID apeared twice!"
         shingles_in_video[video_id] = shingles
 
-        # update list (set) with all shingles
-        all_shingles = np.union1d(shingles, all_shingles)
+        minhashes = []
+        for shingle in shingles: # TODO make sure every shingle apears only once
+            minhash = None
+            for hash_function in hash_functions:
+                newMinhash = hash_function(shingle)
+                if minhash == None or newMinhash < minhash:
+                    minhash = newMinhash
+            minhashes.append(minhash)
 
-    # create hash functions
-    hash_functions = []
-    for i in range(NUM_OF_HASH_FUNCTIONS):
-        hash_functions.append(lambda x: x ^ np.random.randint(1)) # TODO define proper hashing functions
+        lshashes = []
+        for i in range(0, len(minhashes), ROWS_PER_BAND): # what to do with last band?
+            band = minhashes[i:i + ROWS_PER_BAND]
+            band = frozenset(band)
+            lshashes.append(hash(band))
 
-    # MinHashing
-    ## init to inf (None)
-    init_row = [None for i in range(len(shingles_in_video))]
-    signature_matrix = [init_row for j in range(len(all_shingles))]
-
-    # for each column c
-    for c, video_shingles in enumerate(shingles_in_video.iteritems()):
-        video = video_shingles[0]
-        shingles_in_video = video_shingles[1]
-        # for each row r
-        for shingle in all_shingles:
-            # if c has 1 in row r
-            if shingle in shingles_in_video:
-                # for each hash function h_i do
-                for i, hash_function in enumerate(hash_functions):
-                    if signature_matrix[i][c] == None:
-                        signature_matrix[i][c] = hash_function(shingle)
-                    else:
-                        signature_matrix[i][c] = min(signature_matrix[i][c], hash_function(shingle))
-    print signature_matrix
+        print lshashes
